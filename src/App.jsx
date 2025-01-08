0e6fd836adb23a92518e1bbec2860fc84559d387
Tenjing Sherpa <10jingsherpa@gmail.com>
2025-01-08 14:16:19 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -12,43 +11,19 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import Home from "./components/Home";
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import SignUp from "c:/Users/Tenjing/OneDrive/Desktop/BASOBAS/frontend/src/pages/Signup";
-import Profile from "./pages/Profile";
-import Header from "./components/Header";
-import PrivateRoute from "./components/PrivateRoute";
->>>>>>> signInPage
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Header />
-      <Routes>
+      
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/header" element={<Header />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/home" element={<Home />} />
-=======
-    <BrowserRouter>
-      {/* header */}
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-        </Route>
->>>>>>> signInPage
-      </Routes>
-    </BrowserRouter>
+     
+      </Router>
   );
 }
